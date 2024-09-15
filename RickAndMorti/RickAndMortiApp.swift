e88ec2d470c85ea726f1c15aec595e409f7d2219
Martyna <mieczkowska.maartyna@gmail.com>
2024-09-15 21:47:21 +0200

    Initial Commit

---
+++ RickAndMorti/RickAndMortiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortiApp.swift
+//  RickAndMorti
+//
+//  Created by Martyna Mieczkowska on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
