5e5e4a4cac168c83b7dcad5562000ad978abd0c8
Martyna <mieczkowska.maartyna@gmail.com>
2024-10-03 23:04:42 +0200

    Added components - like button

---
+++ RickAndMorti/Views/Components/LikeButton.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct LikeButton: View {
+  @Binding var isLiked: Bool
+  
+  var body: some View {
+    Button(action: {
+      isLiked.toggle()
+    }) {
+      Image(systemName: isLiked ? "heart.fill" : "heart")
+        .foregroundColor(isLiked ? .red : .gray)
+    }
+  }
+}
+
+#if DEBUG
+#Preview {
+  LikeButton(isLiked: .constant(false))
+}
+#endif
