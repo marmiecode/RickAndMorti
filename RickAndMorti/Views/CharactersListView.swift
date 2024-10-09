939e5b5daeef9d6ea99ac72f792b7177531d62eb
Martyna <mieczkowska.maartyna@gmail.com>
2024-10-09 16:59:04 +0200

    Added pikcers for gender, species, status

--- RickAndMorti/Views/CharactersListView.swift
+++ RickAndMorti/Views/CharactersListView.swift
@@ -3,9 +3,61 @@ import SwiftUI
 struct CharactersListView: View {
   @StateObject var viewModel = CharacterViewModel()
   
+  @State private var selectStatus: CharacterStatus? = nil
+  @State private var selectSpecies: CharacterSpecies? = nil
+  @State private var selectGender: CharacterGender? = nil
+  
   var body: some View {
     NavigationView {
       VStack {
+        HStack {
+          Picker("Status", selection: $selectStatus){ Text("Status").tag(CharacterStatus?.none)
+            ForEach(CharacterStatus.allCases, id:\.self) { species in Text(species.description).tag(species as CharacterStatus?)
+            }
+          }
+          .frame(width: 100)
+          .padding()
+          .accentColor(.black)
+          .background(Color.blue.opacity(0.2))
+          .cornerRadius(8)
+          .overlay(
+            RoundedRectangle(cornerRadius: 8)
+              .stroke(Color.blue, lineWidth: 1)
+          )
+          
+          
+          Picker("Species", selection: $selectSpecies) {
+            Text("Species").tag(CharacterSpecies?.none)
+            ForEach(CharacterSpecies.allCases, id:\.self) { species in Text(species.description).tag(species as CharacterSpecies?)
+            }
+          }
+          .frame(width: 100)
+          .padding()
+          .accentColor(.black)
+          .background(Color.blue.opacity(0.2))
+          .cornerRadius(8)
+          .overlay(
+            RoundedRectangle(cornerRadius: 8)
+              .stroke(Color.blue, lineWidth: 1)
+          )
+          
+          Picker("Gender", selection: $selectGender) {
+            Text("Gender").tag(CharacterGender?.none)
+            ForEach(CharacterGender.allCases, id: \.self) { gender in Text(gender.description).tag(gender as CharacterGender?)
+            }
+          }
+          .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+          .padding()
+          .accentColor(.black)
+          .background(Color.blue.opacity(0.2))
+          .cornerRadius(8)
+          .overlay(
+            RoundedRectangle(cornerRadius: 8)
+              .stroke(Color.blue, lineWidth: 1)
+          )
+        }
+        .padding(.horizontal)
+        
         List(viewModel.character) { character in
           NavigationLink(destination: CharacterDetailView(character: character)) {
             HStack {
@@ -24,13 +76,6 @@ struct CharactersListView: View {
                   .font(.subheadline)
                   .lineLimit(1)
               }
-              
-              Spacer()
-              
-              LikeButton(isLiked: Binding(
-                get: { viewModel.isFavourite(character: character) },
-                set: { newValue in viewModel.toggleFavourite(character: character) }
-              ))
             }
           }
         }
