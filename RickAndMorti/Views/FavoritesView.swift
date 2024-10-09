098fc03f1c8c42d704dd127191fb14940c2e7762
Martyna <mieczkowska.maartyna@gmail.com>
2024-10-09 17:53:22 +0200

    Added like button to view

--- RickAndMorti/Views/FavoritesView.swift
+++ RickAndMorti/Views/FavoritesView.swift
@@ -3,31 +3,45 @@ import SwiftUI
 struct FavoritesView: View {
   @ObservedObject var viewModel = CharacterViewModel()
   
-    var body: some View {
-      List(viewModel.favourites) { character in
-        HStack {
-          AsyncImage(url: character.image) { image in image.resizable().scaledToFill()
-          } placeholder: {
-            ProgressView()
-          }
-          .frame(width: 50, height: 50)
-          .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-          
-          VStack(alignment: .leading) {
-            Text(character.name)
-              .font(.headline)
-            Text(character.species.description)
-              .font(.subheadline)
+  var body: some View {
+    VStack {
+      if viewModel.favourites.isEmpty {
+        Text("Empty")
+          .font(.title2)
+          .padding()
+      }else {
+        List(viewModel.favourites) { character in
+          HStack {
+            AsyncImage(url: character.image) { image in image.resizable().scaledToFill()
+            } placeholder: {
+              ProgressView()
+            }
+            .frame(width: 50, height: 50)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             
+            VStack(alignment: .leading) {
+              Text(character.name)
+                .font(.headline)
+              Text(character.species.description)
+                .font(.subheadline)
+              
+            }
+            
+            Spacer()
+            
+            LikeButton(isLiked: Binding(
+              get: { viewModel.isFavourite(character: character) },
+              set: { newValue in viewModel.toggleFavourite(character: character) }
+            ))
           }
-          
-          Spacer()
-          
-          //likebutton
-        }}
+        }
+      }
     }
+  }
 }
 
+#if DEBUG
 #Preview {
-    FavoritesView()
+  FavoritesView()
 }
+#endif
